3c9d2688efc459d864d679bc324e76e61204e48e
Nils Witt <n.witt@zbw.eu>
2014-05-16 12:27:18 +0200

    tapping the archives now.
    due to a lack of beeing able to crawl the past, the spiders have been altered
    to use the json-based archive-interfaces. this is true for the so far proccessed
    blogs: krugman and upshot.

--- Crawler/spiders/krugman.py
+++ Crawler/spiders/krugman.py
@@ -5,37 +5,40 @@ Created on 25.04.2014
 '''
 
 import datetime
+import json
 from scrapy import log
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.selector import Selector
+from scrapy.spider import Spider
 
 from Crawler import toolbox
 from Crawler.items import BlogItem
 from Crawler.toolbox import safepop
 
 
-class MySpider(CrawlSpider):
+class MySpider(Spider):
     name = 'krugman.blogs.nytimes.com'
-    start_urls = ['http://krugman.blogs.nytimes.com/']
+    start_urls = []
+    
+    def __init__(self):
+        log.start()
+        for i in range(1, 100):
+            self.start_urls.append('http://krugman.blogs.nytimes.com/more_posts_json/?homepage=1&apagenum=' + str(i))
 
-    rules = (
-        Rule(SgmlLinkExtractor(restrict_xpaths=('//h3[@class="entry-title"]', )), callback='parse_item'),
-    )
-
-    def parse_item(self, response):
-        sel = Selector(response)
-        item = BlogItem()
-        links = []
-        item['blog_name'] = "The Conscience of a Liberal"
-        item['url'] = response.url
-        item['releasedate'] = safepop(sel.xpath("/html/body/div/main/div[2]/div/div/div/article/header/div[2]/time/@datetime").extract(), 0)
-        item['crawldate'] = datetime.datetime.now().isoformat()
-        item['author'] = "Paul Krugman"
-        item['headline'] = safepop(sel.xpath("/html/body/div/main/div[2]/div/div/div/article/header/h1/text()").extract(), 0)
-        item['body'] = safepop(toolbox.mergeListElements(sel.xpath('//p[@class="story-body-text"]/text() | //p[@class="story-body-text"]/a/text()').extract()), 0)
-        for i in sel.xpath("//p[@class='story-body-text']/a/attribute::href").extract():
-            links.append(i)
-        item['links'] = links
-        log.msg("parsed %s successfully" % response.url, level=log.INFO)
-        return item
+    def parse(self, response):
+        json_response = json.loads(response.body_as_unicode())
+        for i in json_response["posts"]:
+            links = []
+            sel = Selector(text=i['html'])
+            item = BlogItem()
+            item['blog_name'] = "The Conscience of a Liberal"
+            item['url'] = sel.xpath('//div[@data-url]/attribute::data-url').extract()
+            item['releasedate'] = sel.xpath('//time/@datetime').extract()
+            item['crawldate'] = datetime.datetime.now().isoformat()
+            item['author'] = "Paul Krugman"
+            item['headline'] = sel.xpath('//h3[@class="entry-title"]/a/text()').extract()
+            item['body'] = safepop(toolbox.mergeListElements(sel.xpath('//p[@class="story-body-text"]/text() | //p[@class="story-body-text"]/a/text()').extract()), 0)
+            for i in sel.xpath("//p[@class='story-body-text']/a/attribute::href").extract():
+                links.append(i)
+            item['links'] = links
+            log.msg("parsed %s successfully" % response.url, level=log.INFO)
+            yield item
--- Crawler/spiders/upshot.py
+++ Crawler/spiders/upshot.py
@@ -4,42 +4,75 @@ Created on 06.05.2014
 @author: user
 '''
 import datetime
+import json
 from scrapy import log
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.selector import Selector
+from scrapy.spider import Spider
+import urllib2
 
 from Crawler.items import BlogItem
 from Crawler.toolbox import safepop, mergeListElements
 
 
-class MySpider(CrawlSpider):
-    name = 'nytimes.com/upshot'
-    start_urls = ['http://www.nytimes.com/upshot']
-
-    rules = (
-        Rule(SgmlLinkExtractor(restrict_xpaths=('//h2[@class="story-heading stream-item-headline"]', )), callback='parse_item'),
-    )
-    def parse_item(self, response):
+class MySpider(Spider):
+    name = 'nytimes.com/upshot/json'
+    start_urls = []
+    
+    '''
+    the constructor grabs pages of the content delivery interface and
+    extracts the contained url which are pointing to the actual blog-entries.
+    currently it grabs all available entries (~160 on 2014-05-15), there is
+    no upper barrier. it raises an exception (KeyError) if the response
+    won't correspond the expectations.   
+    '''
+    
+    def __init__(self, *args, **kwargs):
+        log.start(loglevel='WARNING', logstdout=False)       
+        try:
+            response = self.fetch_json_doc(1)
+            hits = response['response']['meta']['hits']
+            offset = response['response']['meta']['offset']
+            i = 2
+            while hits > offset:
+                for n in response['response']['docs']:
+                    self.start_urls.append(n['web_url'])
+                    log.msg(n['web_url'], level=log.DEBUG)
+                response = self.fetch_json_doc(i)
+                hits = response['response']['meta']['hits']
+                offset = response['response']['meta']['offset']
+                i += 1                
+        except KeyError:
+            log.msg("Error reading init-Page for spider " + self.name + ".\n\
+#The required response.meta-element was not contained in the response.", level=log.ERROR)
+            raise KeyError("Required Key in dict not found.")
+        super(MySpider, self).__init__(*args, **kwargs)
+        
+    def parse(self, response):
         sel = Selector(response)
         item = BlogItem()
         links = []
         item['blog_name'] = "The Upshot"
         item['url'] = response.url
-        item['releasedate'] = self.parse_date(safepop(sel.xpath("//article/header/div/div/p/time/@datetime | //article/header/div/div/div/p/time/@datetime").extract(), 0))
+        item['releasedate'] = sel.xpath('//p[@class="byline-dateline"]/time[@class="dateline"]/attribute::datetime').extract()
         item['crawldate'] = datetime.datetime.now().isoformat()
-        item['author'] = sel.xpath("//article/div[3]/p/span/a/span/text() | //article/div[3]/p/span/span/text()").extract()                                                                               
-        item['headline'] = safepop(sel.xpath("//article/header/div/h1/text()").extract(), 0)
+        item['author'] = sel.xpath('//span[@class="byline-author"]/text()').extract()                                                                               
+        item['headline'] = sel.xpath('//h1[@class="story-heading" and @itemprop="headline"]/text()').extract()
         item['body'] = safepop(mergeListElements(sel.xpath('//p[@class="story-body-text story-content"]/text() | //p[@class="story-body-text story-content"]/a/text()').extract()), 0)
         for i in sel.xpath("//p[@class='story-body-text story-content']/a/attribute::href").extract():
             links.append(i)
         item['links'] = links
         log.msg("parsed %s successfully" % response.url, level=log.INFO) 
         return item
-
-    def parse_date(self, d):
-        if d is not None:
-            year, month, day = d.split('-')
-            return datetime.datetime(int(year), int(month), int(day), 0, 0).isoformat()
-        else:
-            return None
+        
+    '''
+    the main purpose of this method is to ban the ugly url out of the important
+    elements of the source code. it expects you to pass an int-parameter (otherwise 
+    it will throw an AssertionError) which refers to a page. it returns the response
+    to in a json-style format (a python dict with nested elements). 
+    '''
+    def fetch_json_doc(self, page):
+        assert type(page) == int
+        response = urllib2.urlopen("http://query.nytimes.com/svc/add/v1/sitesearch.json?\
+fq=section_name%3A%28%22The+Upshot%22%29+AND+document_type%3A%28%22article%22%29&page="+ str(page) + "\
+&end_date=" + str(datetime.datetime.today().date()))
+        return json.loads(response.read())
