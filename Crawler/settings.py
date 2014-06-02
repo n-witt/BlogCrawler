0fa494919b6b9a724680cea4a32f6a093348dcbe
Nils Witt <n.witt@zbw.eu>
2014-06-02 13:53:52 +0200

    added spider for brad delong's blog

--- Crawler/settings.py
+++ Crawler/settings.py
@@ -18,6 +18,7 @@ ITEM_PIPELINES = {
 }
 
 COMMANDS_MODULE = 'Crawler.commands'
-USER_AGENT = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36"
+#USER_AGENT = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36"
+USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0"
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'Crawler (+http://www.yourdomain.com)'
--- Crawler/spiders/brad_delong.py
+++ Crawler/spiders/brad_delong.py
@@ -1,5 +1,5 @@
 '''
-Created on 19.05.2014
+Created on 28.05.2014
 @author: nils witt
 '''
 
@@ -8,6 +8,7 @@ from scrapy import log
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.selector import Selector
+from scrapy.utils.response import open_in_browser
 
 from Crawler import toolbox
 from Crawler.items import BlogItem
@@ -16,11 +17,11 @@ import dateutil.parser as dparser
 
 
 class MySpider(CrawlSpider):
-    name = 'marginalrevolution.com'
+    name = 'delong.typepad.com'
     start_urls = []
     rules = (
-        Rule(SgmlLinkExtractor(restrict_xpaths=('//h2[@class="entry-title"]')), callback='parse_item'),
-        Rule(SgmlLinkExtractor(restrict_xpaths=('//div[@class="prev_next"]')), follow=True),
+        Rule(SgmlLinkExtractor(restrict_xpaths=('//h3[@class="entry-header"]/a')), callback='parse_item', follow=True),
+        Rule(SgmlLinkExtractor(restrict_xpaths=('//span[@class="pager-right"]')), follow=True),
     )
 
     def __init__(self, startDate, endDate, *args, **kwargs):
@@ -30,7 +31,7 @@ class MySpider(CrawlSpider):
             raise ValueError(e.message)
         
         for i in range(0, (endDate.year-startDate.year) * 12 + (endDate.month-startDate.month) + 1):
-            url = 'http://marginalrevolution.com/marginalrevolution/' + toolbox.add_months(startDate, i).strftime('%Y/%m')
+            url = 'http://delong.typepad.com/sdj/' + toolbox.add_months(startDate, i).strftime('%Y/%m') + '/'
             self.start_urls.append(url)
         super(MySpider, self).__init__(*args, **kwargs)
 
@@ -38,15 +39,15 @@ class MySpider(CrawlSpider):
         self.log(response.url, level=log.INFO)
         sel = Selector(response)
         item = BlogItem()
-        
-        item['blog_name'] = "MarginalRevolution"
+
+        item['blog_name'] = "Brad DeLong's Grasping Reality..."
         item['url'] = response.url
-        item['releasedate'] = dparser.parse(mergeListElements(sel.xpath('//p[@class="headline_meta"]/text()').extract())[0], fuzzy=True)
+        item['releasedate'] = dparser.parse(mergeListElements(sel.xpath('//h2[@class="date-header"]/text()').extract())[0], fuzzy=True)
         item['crawldate'] = datetime.now().isoformat()
         item['author'] = safepop(sel.xpath('//a[@rel="author"]/text()').extract(), 0)
-        item['headline'] = safepop(sel.xpath('//h1[@class="entry-title"]/text()').extract(), 0)
-        item['body'] = safepop(mergeListElements(sel.xpath('//div[@class="format_text entry-content"]/p[@class="to_comments"]/preceding-sibling::*/descendant-or-self::text()').extract()), 0)
-        item['links'] = sel.xpath('//div[@class="article-content"]/h1[contains(., "Reference")]/following-sibling::*/a/@href').extract()
+        item['headline'] = safepop(sel.xpath('//h3[@class="entry-header"]/text()').extract(), 0)
+        item['body'] = safepop(mergeListElements(sel.xpath('//div[@class="entry-body"]/descendant-or-self::text()').extract()), 0)
+        item['links'] = sel.xpath('//div[@class="entry-body"]/descendant-or-self::a/@href').extract()
         item['references'] = ""
         item['comments'] = ""
         item['tags'] = sel.xpath('//span[@class="post-footers"]/a[@rel="author"]/following-sibling::*/text()').extract()
